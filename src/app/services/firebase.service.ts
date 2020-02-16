8bdf9a26dfe6d44757fa8eb496758d1b39e4e1dc
Davies123 <john.daves13@gmail.com>
2020-02-16 15:50:39 +0100

    added comments to the service file

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 
+
+// imported angularfire for firebase
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 
-
-import * as firebase from 'firebase/app';
-import { AngularFireAuth } from '@angular/fire/auth';
-
+// import map and catchError from rxjs library
 import { map, catchError } from 'rxjs/operators';
 
-
-
+// import Observable from rxjs
 import { Observable } from 'rxjs';
 
 
@@ -23,10 +21,12 @@ export class FirebaseService {
 
   constructor(private afs : AngularFirestore) { }
 
+  // Get item with itemId from collection `Apps`
   getAppDetail(ItemId: string): AngularFirestoreDocument<Item> {
     return this.afs.collection('Apps').doc(ItemId);
   }
 
+  // get snapshot details of specific app with appId
   getApp(id: string): Observable<Item> {
     return this.afs.doc<Item>('Apps/' + id).snapshotChanges()
     .pipe(map(action => {
@@ -36,6 +36,9 @@ export class FirebaseService {
       }));
   }
 
+
+  
+  // get snapshot of all apps in Collection `Apps`
   getApps(): Observable<Item[]> {
     return this.afs.collection<Item>('Apps').snapshotChanges()
     .pipe(map(actions => {
@@ -47,20 +50,25 @@ export class FirebaseService {
     }));
   }
 
+  // get only ids from getApps() function with map from rxjs
   getBookIds(): Observable<String[] | any> {
     return this.getApps()
       .pipe(map(items => items.map(item => item.id)))
       .pipe(catchError(error => error ));
   }
-  
+
+  //get all items from screenshots collection using valueChanges from firebase.
+  // ValueChanges observes the data continuously for changes (CRUD)
   getScreenshots (ItemId: string): Observable<any> {
     return this.afs.collection('Apps').doc(ItemId).collection('screenshots').valueChanges();
   }
 
+  // get ratings which also contains comments for every itemId with subcollection `comments`
   getRatingsDetail ( ItemId: string): AngularFirestoreCollection<Item> {
     return this.afs.collection('Apps').doc(ItemId).collection('comments');
   }
 
+  // get snapshot data for all subcollection (comments) for a specific ItemId
   getComments(ItemId: any): Observable<any> {
     return this.afs.collection('Apps').doc(ItemId).collection('comments')
     .snapshotChanges().pipe(
